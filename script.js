54159e8fdf0d653c14600f67008df6cb93eebde0
machariav722-droid <machariav722@gmail.com>
2026-08-07 18:03:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function generateAkanName() {
 
-    
     const day = parseInt(document.getElementById("day").value);
     const month = parseInt(document.getElementById("month").value);
     const year = parseInt(document.getElementById("year").value);
